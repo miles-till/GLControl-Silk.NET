a0e7e65350c0838200b1d84f649dabf4ed369aa4
Miles Till <till.miles@gmail.com>
2023-06-09 09:24:31 +1000

    Copy from OpenTK.WinForms

---
+++ GLControlSilk.WinForms.TestForm/Form1.cs
@@ -0,0 +1,116 @@
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+
+namespace GLControlSilk.WinForms.TestForm
+{
+    public partial class Form1 : Form
+    {
+        private System.Windows.Forms.Timer _timer = null!;
+        private float _angle = 0.0f;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void glControl_Load(object sender, EventArgs e)
+        {
+            // Make sure that when the GLControl is resized or needs to be painted,
+            // we update our projection matrix or re-render its contents, respectively.
+            glControl.Resize += glControl_Resize;
+            glControl.Paint += glControl_Paint;
+
+            // Redraw the screen every 1/20 of a second.
+            _timer = new System.Windows.Forms.Timer();
+            _timer.Tick += (sender, e) =>
+            {
+                _angle += 0.5f;
+                Render();
+            };
+            _timer.Interval = 50;   // 1000 ms per sec / 50 ms per frame = 20 FPS
+            _timer.Start();
+
+            // Ensure that the viewport and projection matrix are set correctly initially.
+            glControl_Resize(glControl, EventArgs.Empty);
+        }
+
+
+        private void glControl_Resize(object? sender, EventArgs e)
+        {
+            glControl.MakeCurrent();
+
+            if (glControl.ClientSize.Height == 0)
+                glControl.ClientSize = new System.Drawing.Size(glControl.ClientSize.Width, 1);
+
+            GL.Viewport(0, 0, glControl.ClientSize.Width, glControl.ClientSize.Height);
+
+            float aspect_ratio = Math.Max(glControl.ClientSize.Width, 1) / (float)Math.Max(glControl.ClientSize.Height, 1);
+            Matrix4 perpective = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspect_ratio, 1, 64);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perpective);
+        }
+
+        private void glControl_Paint(object? sender, PaintEventArgs e)
+        {
+            Render();
+        }
+
+        private void Render()
+        {
+            glControl.MakeCurrent();
+
+            GL.ClearColor(Color4.MidnightBlue);
+            GL.Enable(EnableCap.DepthTest);
+
+            Matrix4 lookat = Matrix4.LookAt(0, 5, 5, 0, 0, 0, 0, 1, 0);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref lookat);
+
+            GL.Rotate(_angle, 0.0f, 1.0f, 0.0f);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.Begin(PrimitiveType.Quads);
+
+            GL.Color4(Color4.Silver);
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+
+            GL.Color4(Color4.Honeydew);
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+
+            GL.Color4(Color4.Moccasin);
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+
+            GL.Color4(Color4.IndianRed);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+
+            GL.Color4(Color4.PaleVioletRed);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+
+            GL.Color4(Color4.ForestGreen);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+
+            GL.End();
+
+            glControl.SwapBuffers();
+        }
+    }
+}
